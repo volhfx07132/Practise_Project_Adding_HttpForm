1780d0d9bf6edd6d3105959b871813458ffb3843
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-09 16:59:42 +0700

    Fetching Meals via Http

---
+++ my-react-app/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fectchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-9517f-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fectchMeals();
+  });
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
